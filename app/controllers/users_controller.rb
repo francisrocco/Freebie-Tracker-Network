c58ea55423864766fc2bcd917d1cae38634be40b
Francis <roccofrancis@gmail.com>
2016-08-27 16:53:49 -0400

    add controllers and corresponding actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
--- db/migrate/20160826201328_create_freebies.rb
+++ db/migrate/20160826201328_create_freebies.rb
@@ -3,7 +3,7 @@ class CreateFreebies < ActiveRecord::Migration[5.0]
     create_table :freebies do |t|
       t.string :name
       t.integer :market_value
-      t.belongs_to :brand, foreign_key: true
+      t.belongs_to :brand, foreign_key: true, optional: true
 
       t.timestamps
     end
