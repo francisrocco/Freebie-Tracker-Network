4ab2369d21043f882f09349b791ae5f97cd99711
jasmin47 <alxsanborn@gmail.com>
2016-10-22 10:27:43 -0400

    add user method tests, refactor user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,13 @@ require "rails_helper"
 require 'byebug'
 
 RSpec.describe User, :type => :model do
+  let(:alex) {User.create(name: "Alex", email: "alxsanborn@gmail.com", password: "1234")}
+  let(:brand) {Brand.create(name: "William-Sonoma", prestige: 4, location: "123 Mulberry Street", business: alex)}
+  let(:item_1) {Item.create(name: "cat mug", market_value: 20, brand_id: brand.id)}
+  let(:item_2) {Item.create(name: "lotion", market_value: 20, brand_id: brand.id)}
 
   context 'validations' do
     it 'should be valid with a name, email, and password' do
-      alex = User.new(name: "Alex", email: "alxsanborn@gmail.com", password: "1234")
       expect(alex).to be_valid
     end
 
@@ -22,15 +25,22 @@ RSpec.describe User, :type => :model do
 
   context 'relationships' do
     it 'a user can have many items' do
-      alex = User.create(name: "Alex", email: "alxsanborn@gmail.com", password: "1234")
-      brand = Brand.create(name: "William-Sonoma", prestige: 4, location: "123 Mulberry Street", business: alex)
-      item_1 = Item.create(name: "cat mug", market_value: 20, brand_id: brand.id)
-      item_2 = Item.create(name: "lotion", market_value: 20, brand_id: brand.id)
-      got_it_1 = UserItem.create(user_id: alex.id, item_id: item_1.id)
-      got_it_2 = UserItem.create(user_id: alex.id, item_id: item_2.id)
-      
+      UserItem.create(user_id: alex.id, item_id: item_1.id)
+      UserItem.create(user_id: alex.id, item_id: item_2.id)
       expect(alex.items.first).to eq(item_1)
       expect(alex.items.last).to eq(item_2)
+    end
+  end
+
+  context 'method tests' do
+    it 'calculates total market value of all freebies that a user has collected and recognizes status level' do
+      UserItem.create(user_id: alex.id, item_id: item_1.id)
+      UserItem.create(user_id: alex.id, item_id: item_2.id)
+      total = alex.total_market_value
+      status = alex.status_level
+
+      expect(total).to eq(40)
+      expect(status).to eq("Newbie")
     end
   end
 end
