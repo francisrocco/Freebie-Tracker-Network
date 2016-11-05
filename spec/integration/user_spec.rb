a78a557aaa84aee18972f6527db20b5b00de5a43
jasmin47 <alxsanborn@gmail.com>
2016-11-05 19:02:43 -0400

    add capybara tests for users to add and delete items

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -1,6 +1,26 @@
-# session = Capybara::Session.new(:webkit, my_rack_app)
-# session.within("form#session") do
-#   session.fill_in 'Email', with: 'user@example.com'
-#   session.fill_in 'Password', with: 'password'
-# end
-# session.click_button 'Sign in'
+require 'spec_helper'
+
+feature "Capybara: user interface", :js => true do
+  before :each do
+    visit root_path
+    page.find("#sign-in").click
+    fill_in 'user_email', with: "clay@gmail.com"
+    fill_in 'user_password', with: '1234'
+    click_button "submit"
+  end
+
+
+  scenario 'user can view items index page and add item' do
+    visit items_path
+    expect(page).to have_text('All the Freebies!')
+
+    first('.freebie-item').click_button("Add to my freebie collection")
+    expect(page).to have_text('Chanel logo keychain')
+  end
+
+  scenario 'user can delete item' do
+    first('.user-freebie').click_button("X")
+    expect(page).to_not have_text('Burrito')
+  end
+
+end
